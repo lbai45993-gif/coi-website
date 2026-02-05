ece39413c4b10765f5a06e38ae7acaa549cb6ca5
Yusen121 <1548429568@qq.com>
2026-02-04 19:28:10 -0800

    Fix vercel adapter config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://lbai45993-gif.github.io",
   base: "/coi-website/",
   output: "static",
+
+  adapter: vercel({}),
 
   vite: {
     plugins: [tailwindcss()],
